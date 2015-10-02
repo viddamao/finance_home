b9c0258691523a4f408043be7f2245c590009ef2
wm56 <wm56>
2015-10-02 19:12:02 -0400

    fixed ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,11 +9,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/index', function(request, response) {
   response.render('pages/index');
 });
 
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+
+app.listen(8080);
+  
+console.log('Node app is running on port', app.get('port'));
+
