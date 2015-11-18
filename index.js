22ddac6e2df4f0321ab1da9f8a45b984cdff384e
wm56 <wm56>
2015-11-17 23:55:50 -0500

    bug fix on port

--- index.js
+++ index.js
@@ -171,7 +171,27 @@ mongoose.connect(uristring, function (err, res) {
 		console.dir(coke);
 	});
 	
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+	
 var port = normalizePort(process.env.PORT || '8080');
 app.set('port', port));
 
