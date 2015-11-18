af2254511a761a7eb740e8d13e307c9421e9514a
wm56 <wm56>
2015-11-18 01:32:57 -0500

    some minor changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log('render homepage');
-	res.render('index','pages/index');
+	res.render('pages/index');
 });
 
 module.exports = router;
