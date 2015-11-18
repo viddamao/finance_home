e29226efaeb27e2b74dcd9694bc0fb84f6b473f3
wm56 <wm56>
2015-11-18 02:44:36 -0500

    try about page

---
+++ routes/about.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	console.log('render about page');
+	res.render('pages/about');
+});
+
+module.exports = router;
