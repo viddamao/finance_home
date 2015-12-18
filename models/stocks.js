328715ed0c4706e87a1b9e300d540cf5390cba2a
wm56 <wm56>
2015-12-18 02:27:23 -0500

    execute once

--- models/stocks.js
+++ models/stocks.js
@@ -2,4 +2,81 @@ var mongoose = require('mongoose');
 var stockSchema = require('../schemas/stocks');
 var Stocks = mongoose.model('stocks', stockSchema);
 
+var fs = require('fs');
+
+function readLines(input, add_stock) {
+  var remaining = ''	//remaining input stream
+  
+
+  
+  //process and splice stock input
+    remaining += input;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+	  
+	  add_stock(line);
+	  
+	  index = remaining.indexOf('\n');
+	 
+      
+    }
+
+    if (remaining.length > 0) {
+      add_stock(remaining);
+    }
+ 
+}
+
+function add_stock(data) {
+	var id = '';			//stock id
+	var name = '';			//stock name
+	var abbr = '';			//stock abbr
+	var first_split = 0;	
+	var second_split = 0;
+  
+	first_split = data.indexOf(';');
+	second_split = data.substring(first_split+1).indexOf(';');
+	var temp = data.substring(second_split+1);
+	temp=temp.substring(temp.indexOf(';')+1);
+	var split = temp.indexOf(';');	
+	  
+	id = data.substring(0,first_split);
+	name = temp.substring(0,split);
+	abbr = temp.substring(split+1);
+	
+	
+	//console.log(id,'  ',str,'  ',abbr,'  ',name);
+		
+	var new_stock = new Stock({
+		"name" 	: name,
+        "id" 	: id,
+		"abbr"	: abbr	
+	});  
+	
+	new_stock.save(function(err, new_stock) {
+		if (err) return console.error(err);
+		//console.dir(new_stock);
+	});
+}
+
+var stock_init = false;
+
+if (stock_init==false){
+Stock.remove({}, function(err) { 
+   console.log('collection removed') 
+});
+
+var iconv = require('iconv-lite'); 
+var bin = fs.readFileSync('stockList_20151217.txt');
+
+    if (bin[0] === 0xEF && bin[1] === 0xBB && bin[2] === 0xBF) {
+        bin = bin.slice(3);
+    }
+
+readLines(bin.toString('utf-8'),add_stock);
+stock_init=true; 
+}
+
 module.exports = Stocks;
