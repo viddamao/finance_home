26557388c9d2d50b4277fedb775a4a1142934e66
wm56 <wm56>
2015-12-18 19:08:00 -0500

    add alphabet search

--- routes/search.js
+++ routes/search.js
@@ -8,12 +8,18 @@ var article = require('../models/articles');
 /* GET article page. */
 router.post('/stocks', function(request, response) {
 	console.log('render stocks page');
-	var userQuery = request.body;
+	var userQuery = request.body.toUpperCase();
 	console.log(userQuery);
+	
+	if ((userQuery.charCodeAt(0)>=48)&(userQuery.charCodeAt(0)<=57)){				//is id search
+		
+	
 	//var userInput = localStorage.getItem("stockId");
-	var stockQuery = stock.findOne({ id: userQuery.userInputStockId },"name id articles",function (err, result) {
+	var stockQuery = stock.findOne({ id: userQuery.userInputStockId },"name id articles abbr",function (err, result) {
 	if (err) // handle this
 		console.log("can't find stock in database");
+	
+	
 	
 	var articleQuery = article.find({stock_uid: userQuery.userInputStockId},"author_name title href date likes",function (err, articleResult) {
 	if (err) // handle this
@@ -37,8 +43,42 @@ router.post('/stocks', function(request, response) {
 	
 	
 	});
+	}
+	else if ((userQuery.charCodeAt(0)>=65)&(userQuery.charCodeAt(0)<=90)){		//is abbr search
+		
+	var stockQuery = stock.findOne({ abbr: userQuery.userInputStockId },"name id articles abbr",function (err, result) {
+	if (err) // handle this
+		console.log("can't find stock in database");
 	
 	
+	
+	var articleQuery = article.find({stock_abbr: userQuery.userInputStockId},"author_name title href date likes",function (err, articleResult) {
+	if (err) // handle this
+		console.log("can't find article in database");
+	
+	if (result == null){
+		
+	response.render('pages/error',userQuery);	
+		
+	}
+		
+	var stockVariables = {
+		name: result.name,
+		id :result.id,
+		abbr : result.abbr,
+		articles:articleResult
+	};
+	
+	response.render('pages/stocks',stockVariables);	
+	
+	});
+	
+	
+	});
+	}
+	else{
+		response.render('pages/error',userQuery);	
+	}
 	
 });
 
