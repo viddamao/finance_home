12b6346fbf145083d0b8281916ba9462803371d6
wm56 <wm56>
2015-12-23 02:25:57 -0500

    add log in check

--- routes/login.js
+++ routes/login.js
@@ -44,4 +44,21 @@ app.get('/logout', checkLogin);
     req.flash('success', '登出成功!');
     res.redirect('/');//登出成功后跳转到主页
   });
+  
+ function checkLogin(req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录!'); 
+      res.redirect('/login');
+    }
+    next();
+  }
+
+  function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录!'); 
+      res.redirect('back');//返回之前的页面
+    }
+    next();
+  }
+}; 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -50,4 +50,22 @@ app.get('/signup', checkNotLogin);
       });
     });
   });
+  
+function checkLogin(req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录!'); 
+      res.redirect('/login');
+    }
+    next();
+  }
+
+  function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录!'); 
+      res.redirect('back');//返回之前的页面
+    }
+    next();
+  }
+};
+  
 module.exports = router;
