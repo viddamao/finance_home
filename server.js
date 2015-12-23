5c5fb1373534844eb550af24ad1ba31ba46fafcf
wm56 <wm56>
2015-12-23 03:16:31 -0500

    fix error

--- server.js
+++ server.js
@@ -96,12 +96,14 @@ app.use("*",function(req, res, next) {
 
 // production error handler
 // no stacktraces leaked to user
+
+/*
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('./pages/error');
 });
 
-
+*/
 
 
 /*
