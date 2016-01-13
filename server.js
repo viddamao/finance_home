e108940362c2a9f8583ab64ea5024fdc1aef37ba
wm56 <wm56>
2016-01-13 07:59:01 -0500

    fix error handling

--- server.js
+++ server.js
@@ -50,16 +50,6 @@ app.use('/', articles);
 
 
 
-// catch 404 and forward to error handler
-
-app.use("*",function(req, res, next) {
-	console.log("404");
-	var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-
 // production error handler
 // no stacktraces leaked to user
 
