36c3b5663ea036ad93d42af25753e6019a9c4bad
wm56 <wm56>
2016-01-13 14:32:13 -0500

    a

--- public/js/jquery.tablesorter.js
+++ public/js/jquery.tablesorter.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 $(function(){
+
+var $table = $('table'),
+  // define pager options
+  pagerOptions = {
+    // target the pager markup - see the HTML block below
+    container: $(".pager"),
+    // output string - default is '{page}/{totalPages}';
+    // possible variables: {size}, {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+    // also {page:input} & {startRow:input} will add a modifiable input in place of the value
+    output: '{startRow} - {endRow} / {filteredRows} ({totalRows})',
+    // if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
+    // table row set to a height to compensate; default is false
+    fixedHeight: true,
+    // remove rows from the table to speed up the sort of large tables.
+    // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+    removeRows: false,
+    // go to page selector - select dropdown that sets the current page
+    cssGoto: '.gotoPage'
+  };	
+	
 $("#mainTable").tablesorter({
 
     // *** APPEARANCE ***
