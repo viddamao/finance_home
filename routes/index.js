ffc803b83f2fb2f1dc6565b9d7e78a4940c77901
wm56 <wm56>
2016-01-13 07:56:16 -0500

    modified css

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+/*
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   console.log("/"+req.method);
   next();
 });
+*/
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
