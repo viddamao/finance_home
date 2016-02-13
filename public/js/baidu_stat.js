a1e0eea9293b94e10ecb132814319ba5a913cac3
Vidda <viddamaowj@hotmail.com>
2016-02-13 23:29:27 +0800

    w3c

---
+++ public/js/baidu_stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?1647ea7c9459835637b3f278c81f4ae6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ public/js/screen.js
@@ -0,0 +1,36 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?1647ea7c9459835637b3f278c81f4ae6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+$(function(){
+	 $( "#size-slider" ).slider({
+      range: true,
+      min: 0,
+      max: 10000,
+      values: [ 0, 10000 ],
+      slide: function( event, ui ) {
+        $( "#size-val" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#size-val" ).val( "$" + $( "#size-slider" ).slider( "values", 0 ) +
+      " - $" + $( "#size-slider" ).slider( "values", 1 ) );
+	  
+	 $( "#since-slider" ).slider({
+      range: true,
+      min: 0,
+      max: 15,
+      values: [ 0, 15 ],
+      slide: function( event, ui ) {
+        $( "#since-val" ).val(  ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+      }
+    });
+    $( "#since-val" ).val( "$" + $( "#since-slider" ).slider( "values", 0 ) +
+      " - $" + $( "#since-slider" ).slider( "values", 1 ) );
+	  
+	
+});
