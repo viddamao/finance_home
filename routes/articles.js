8e8eaa165f90f027683785ac6caed4819a3f4ca6
Vidda <viddamaowj@hotmail.com>
2016-02-14 16:33:33 +0800

    a

--- routes/articles.js
+++ routes/articles.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET article page. */
 router.get('/articles', function(req, res) {
 	console.log('render articles page');
-	res.render('./public/show_enai/index', function(err, html) {
+	res.render('../public/show_enai/index', function(err, html) {
 		res.send(html);
 	});
 });
