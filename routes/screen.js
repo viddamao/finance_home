f43c1e209c76a576533b46c6e0ba8d12757d21e3
Vidda <viddamaowj@hotmail.com>
2016-02-16 00:38:58 +0800

    a

--- routes/screen.js
+++ routes/screen.js
@@ -5,12 +5,12 @@ var router = express.Router();
 /* GET screen page. */
 router.get('/screen', function(req, res) {
 	console.log('render screen page');
-	exports.funds = function(db) {
+	exports.users = function(db) {
     return function(req, res) {
-        var collection = db.get('funds'); //获得数据库中的集合(类似关系数据库中的表)
+        var collection = db.get('users'); //获得数据库中的集合(类似关系数据库中的表)
         collection.find({},{},function(e,docs){ //取得所有的集合数据, 渲染到页面上,关键字是userlist
             res.render('./pages/screen', {
-                "funds" : docs
+                "users" : docs
             },function(err, html) {
 				res.send(html);
 			});
