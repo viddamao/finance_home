30e625ef68040a2932244a7a992b931c26369efa
Vidda <viddamaowj@hotmail.com>
2016-02-15 18:13:05 +0800

    a

--- models/fund.js
+++ models/fund.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fundSchema = require('../schemas/fund');
-var fund = mongoose.model('fund', fundSchema);
+var Fund = mongoose.model('fund', fundSchema);
 
 
 module.exports = Fund;
