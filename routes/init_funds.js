617fabaa615c4970f53f78665f733f8fc6583cb8
Vidda <viddamaowj@hotmail.com>
2016-02-15 19:23:59 +0800

    a

--- routes/init_funds.js
+++ routes/init_funds.js
@@ -11,7 +11,7 @@ function readLines(input, add_fund) {
     while (index > -1) {
       var line = remaining.substring(0, index);
       remaining = remaining.substring(index + 1);
-      console.log(line.toString());
+      console.log(line);
 	  //add_fund(line);
 	  index = remaining.indexOf('\n');
       
